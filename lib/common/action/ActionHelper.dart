5caf2f75a493c38820145af7719013548a169044
yun <>
2020-02-18 17:16:13 +0800

    reform ActionHelper

--- lib/common/action/ActionHelper.dart
+++ lib/common/action/ActionHelper.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
 class ActionHelper {
-  static Future<int> showAction(BuildContext context, List<String> actions) async {
+  static Future<int> showIOSAction(
+    BuildContext context,
+    List<String> actions, {
+    String title,
+    String message,
+  }) async {
     int i = await showCupertinoModalPopup<int>(
         context: context,
         builder: (BuildContext context) {
           return CupertinoActionSheet(
-            title: const Text('请选择主题'),
-//            message: const Text('请选择语言'),
+            title: title == null ? null : Text(title ?? "选择"),
+            message: message == null ? null : Text(message ?? "选择"),
             cancelButton: CupertinoActionSheetAction(
               onPressed: () {
                 Navigator.pop(context, null);
